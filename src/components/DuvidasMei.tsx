0dacf3cc9653d906e54d7f5806d996fafce856ef
Jhoelber <116396136+Jhoelber@users.noreply.github.com>
2025-11-16 21:17:23 -0300

    Update DuvidasMei.tsx

--- src/components/DuvidasMei.tsx
+++ src/components/DuvidasMei.tsx
@@ -266,13 +266,14 @@ Não use colchetes [ ] ou parênteses ( ) ao enviar links. Sempre envie os links
                             <div className="sticky bottom-0 -mx-8 mt-4 border-t border-slate-200 dark:border-slate-800 
                     bg-white/90 dark:bg-slate-900/90 backdrop-blur px-8 py-4">
                                 <div className="flex gap-3">
-                                    <button
+                                    {/*  <button
                                         // onClick={() => navigator.clipboard.writeText(resultado || "")}
                                         className="inline-flex items-center justify-center rounded-lg border border-slate-300 bg-white text-slate-900 px-4 py-2 text-sm font-medium hover:bg-slate-50 disabled:opacity-50 dark:bg-slate-900 dark:text-slate-100 dark:border-slate-700 dark:hover:bg-slate-800"
                                         disabled={!resultado}
                                     >
                                         Enviar para -mail
                                     </button>
+                                    */}
                                     <button
                                         onClick={() => setResultado("")}
                                         className="inline-flex items-center justify-center rounded-lg border border-transparent bg-slate-100 text-slate-700 px-4 py-2 text-sm font-medium hover:bg-slate-200 disabled:opacity-50 dark:bg-slate-800 dark:text-slate-200 dark:hover:bg-slate-700"
